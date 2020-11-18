8c27a8bf1ef2193e4b53468a60b283c9ed83261a
Fabio Votta <fabiovotta@Air-van-Fabio.fritz.box>
2020-11-18 01:17:57 +0100

    change it to get posts again

--- script.py
+++ script.py
@@ -76,7 +76,7 @@ def insta_posts_py(query, scope, max_posts, scrape_comments, save_path = "", sin
 			if scope == "hashtag":
 				query = query.replace("#", "")
 				hashtag_obj = instaloader.Hashtag.from_name(instagram.context, query)
-				chunk = hashtag_obj.get_all_posts()
+				chunk = hashtag_obj.get_posts()
 			elif scope == "username":
 				query = query.replace("@", "")
 				profile = instaloader.Profile.from_username(instagram.context, query)
